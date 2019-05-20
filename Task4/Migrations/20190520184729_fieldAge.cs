57c4993cc690b173131ff4cd173b3267b89d8345
Kate <momiken98@gmail.com>
2019-05-21 01:59:31 +0700

    added age to employee

---
+++ Task4/Migrations/20190520184729_fieldAge.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task4.Migrations
+{
+    public partial class fieldAge : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Employees",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Employees");
+        }
+    }
+}
--- Task4/Models/Employee.cs
+++ Task4/Models/Employee.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 namespace Task4.Models{
     public class Employee{
         public int EmployeeId { get; set; }
+        public int Age { get; set; }
         public string FullName { get; set; }
         public ICollection<Project> Projects { get; set; }
     }
