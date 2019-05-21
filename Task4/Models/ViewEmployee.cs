076c89a425ddd8a0b29d398a454cd9b6e59ca50a
Kate <momiken98@gmail.com>
2019-05-21 10:32:39 +0700

    age visible

--- Task4/Models/ViewEmployee.cs
+++ Task4/Models/ViewEmployee.cs
@@ -13,6 +13,7 @@ namespace Task4.Models
             this.FullName = e.FullName;
             this.EmployeeId = e.EmployeeId;
             this.Projects = e.Projects;
+            this.Age = e.Age;
         }
 
         public int PremiumSum { get; set; }
